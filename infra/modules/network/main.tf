ecf1d3d66b7112895f04f851d88755172bec71b2
Priyansh Chaurasiya <154089298+priyanshch1105@users.noreply.github.com>
2026-01-21 22:54:32 +0530

    Update main.tf

--- infra/modules/network/main.tf
+++ infra/modules/network/main.tf
@@ -1,8 +1,8 @@
 resource "aws_vpc" "this" {
-  cidr_block = var.cidr_block
+  cidr_block = local.cidr_block
 
   tags = {
-    Name        = "${var.env}-vpc"
-    Environment = var.env
+    Name        = "${local.env}-vpc"
+    Environment = local.env
   }
 }
